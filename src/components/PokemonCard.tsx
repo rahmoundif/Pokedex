2a0b64e728a9162fa3d40a5f5f72326566491abe
Rahmoun Dif <rahmoun.dif@gmail.com>
2025-03-31 22:28:08 +0200

    component.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,24 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon picture" /><figcaption>Bulbasaure</figcaption></figure>;
-    
-  }
-  
+  const pokemon = pokemonList[1];
+  const pokemonSrc = pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>;
+  return (
+    <figure>
+      {pokemonSrc}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  ); 
+  };
+
   export default PokemonCard;
+
